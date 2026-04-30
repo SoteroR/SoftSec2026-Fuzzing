0bf6d36ece15398a447689670dec530e78d14108
mikhailperevoznyk <xlneoxl@gmail.com>
2026-04-30 17:00:02 +0200

    main changes for audio function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_AUDIO) != 0) {
         return 1;
     }
 
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) != 0) {
+        return 1;
+    }
+
+
+    Mix_CloseAudio();
     SDL_Quit();
     return 0;
 }
