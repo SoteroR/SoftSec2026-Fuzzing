cc987151bc6e57953af7cb455c0c2a93e49e4b67
sotero <sprm.04@gmail.com>
2026-05-05 16:07:22 +0200

    harness try, i didnt test it

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-    if (SDL_Init(SDL_INIT_AUDIO) != 0) {
+
+int main(int argc, char **argv) {
+
+    //change depending on how many arguments we want (so far, just filenmae)
+    if (argc != 2) {
+        fprintf(stderr,"filename missing");
+        return 2;
+    }
+
+    SDL_AudioSpec spec;
+    Uint8 *audio_buf;
+    Uint32 audio_len;
+
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        return 1;
+    }
+    //target funciton
+    if (SDL_LoadWAV(argv[1], &spec, &audio_buf, &audio_len) == NULL) {
+        SDL_Quit();
         return 1;
     }
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) != 0) {
-        return 1;
-    }
-
-
-    Mix_CloseAudio();
+    SDL_FreeWAV(audio_buf);
     SDL_Quit();
     return 0;
 }
